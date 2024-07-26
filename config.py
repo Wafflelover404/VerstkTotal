a48b3f792c5cab082b36bf7b6535d5a2065392a0
John Afanasieff <afanasieffivan@gmail.com>
2024-07-26 18:06:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Configure everything here")
-st.markdown("Don't worry, we respect your privacy 🤫")
+st.markdown("Don't worry, we respect your privacy 🤫", help="All tokens are stored locally in session and accessible by user only.")
 
 st.header("Configure your tokens", help="User Access Tokens are the preferred way to authenticate an application or notebook to Hugging Face services. You can manage your access tokens in your settings.")
 
@@ -15,8 +15,8 @@ px = st.text_input("Enter your Pixabay token 🖼️", value=st.session_state.ge
 if px:
     st.session_state["px_token"] = px
 
-st.header("Code assistant settings")
-ctxt = st.checkbox("Code context", help="Tick the checkbox to let AI see your code.", value=st.session_state.get("context", False))
+st.header("Code editor settings")
+ctxt = st.checkbox("Ai code context", help="Tick the checkbox to let AI assistant see your code.", value=st.session_state.get("context", False))
 if 'context' not in st.session_state:
     st.session_state["context"] = ctxt
 else:
